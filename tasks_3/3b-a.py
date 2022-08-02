e1c378e743f9de624e0c04d4282d034a46d56f4c
Leonid Starykh <staryhleonid@gmail.com>
2022-08-02 13:08:30 +0300

    Delete tasks_3 directory

--- tasks_3/3b-a.py
+++
@@ -1,8 +0,0 @@
-arr1 = set(list(map(int, input().split())))
-arr2 = set(list(map(int, input().split())))
-
-cnt = 0
-for i in arr1:
-    if i in arr2:
-        cnt += 1
-print(cnt)
--- tasks_3/3b-b.py
+++
@@ -1,9 +0,0 @@
-arr = list(map(int, input().split()))
-dict = {}
-
-for i in range(len(arr)):
-    if dict.get(arr[i]):
-        print('YES')
-    else:
-        dict[arr[i]] = True
-        print('NO')
