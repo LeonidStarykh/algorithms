9217bf4f0d5649e4b75f4608cd7781e8ffb87faa
Leonid Starykh <staryhleonid@gmail.com>
2022-08-02 13:07:59 +0300

    Delete tasks_1 directory

--- tasks_1/1b-c.py
+++
@@ -1,6 +0,0 @@
-x, y, z = map(int, input().split())
-
-if (1 <= x <= 12) and (1 <= y <= 12) and (x != y):
-    print(0)
-else:
-    print(1)
--- tasks_1/1b-d.py
+++
@@ -1,4 +0,0 @@
-N = int(input())
-houses = list(map(int, input().split()))
-
-print(houses[N//2])
